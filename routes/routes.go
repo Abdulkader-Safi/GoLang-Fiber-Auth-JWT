f1c641c7262376a0c341aa6de2c951ce649833aa
Abdulkader Safi <53955839+Abdulkader-Safi@users.noreply.github.com>
2024-05-18 00:22:03 +0300

    setup Controllers

--- routes/routes.go
+++ routes/routes.go
@@ -1,9 +1,10 @@
 package routes
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/Abdulkader-Safi/Go-Auth-jwt-Fiber/controllers"
+	"github.com/gofiber/fiber/v2"
+)
 
 func Setup(app *fiber.App) {
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello World")
-	})
+	app.Get("/", controllers.Hello)
 }
