7285467b078ada16460421f8a90b1ec93bf7715b
Abdulkader Safi <53955839+Abdulkader-Safi@users.noreply.github.com>
2024-05-18 00:34:31 +0300

    Setup User Model

--- database/connection.go
+++ database/connection.go
@@ -1,13 +1,16 @@
 package database
 
 import (
+	"github.com/Abdulkader-Safi/Go-Auth-jwt-Fiber/models"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func Connect() {
-	_, err := gorm.Open(mysql.Open("abdulkader:Safi@2020@/goauth"), &gorm.Config{})
+	connection, err := gorm.Open(mysql.Open("abdulkader:Safi@2020@/goauth"), &gorm.Config{})
 	if err != nil {
 		panic("Could not connect to database")
 	}
+
+	connection.AutoMigrate(&models.User{})
 }
