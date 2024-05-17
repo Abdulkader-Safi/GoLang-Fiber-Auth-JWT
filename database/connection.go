97deaf39bbe9525d6ac51924c74e0146ae0a2444
Abdulkader Safi <53955839+Abdulkader-Safi@users.noreply.github.com>
2024-05-18 00:41:51 +0300

    Save user to database

--- database/connection.go
+++ database/connection.go
@@ -6,11 +6,15 @@ import (
 	"gorm.io/gorm"
 )
 
+var DB *gorm.DB
+
 func Connect() {
 	connection, err := gorm.Open(mysql.Open("abdulkader:Safi@2020@/goauth"), &gorm.Config{})
 	if err != nil {
 		panic("Could not connect to database")
 	}
+
+	DB = connection
 
 	connection.AutoMigrate(&models.User{})
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.22.0
 
 require (
 	github.com/gofiber/fiber/v2 v2.52.4
+	golang.org/x/crypto v0.14.0
 	gorm.io/driver/mysql v1.5.6
 	gorm.io/gorm v1.25.10
 )
