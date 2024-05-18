8e9129320b4bb760ed262dac5a3264f330fe0d7a
Abdulkader Safi <53955839+Abdulkader-Safi@users.noreply.github.com>
2024-05-18 09:13:09 +0300

    get user data by token from cookie

--- routes/routes.go
+++ routes/routes.go
@@ -7,6 +7,7 @@ import (
 )
 
 func Setup(app *fiber.App) {
-	app.Get("/api/register", controllers.Register)
-	app.Get("/api/login", controllers.Login)
+	app.Post("/api/register", controllers.Register)
+	app.Post("/api/login", controllers.Login)
+	app.Get("/api/user", controllers.User)
 }
