8dedd88af08f74dd656be38ce149b6fcc25c4ea6
Abdulkader Safi <53955839+Abdulkader-Safi@users.noreply.github.com>
2024-06-29 17:18:45 +0300

    Add PostgreSQl Connection Streing

--- Backend/database/connection.go
+++ Backend/database/connection.go
@@ -1,16 +1,19 @@
 package database
 
+// "gorm.io/driver/mysql"
+
 import (
 	"github.com/Abdulkader-Safi/Go-Auth-jwt-Fiber/models"
 
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
 func Connect() {
-	connection, err := gorm.Open(mysql.Open("abdulkader:Safi@2020@/goauth"), &gorm.Config{})
+	// connection, err := gorm.Open(mysql.Open("abdulkader:Safi@2020@/goauth"), &gorm.Config{})
+	connection, err := gorm.Open(postgres.Open("postgresql://postgres:Safi@2020@localhost:5430/grr"), &gorm.Config{})
 	if err != nil {
 		panic("Could not connect to database")
 	}
